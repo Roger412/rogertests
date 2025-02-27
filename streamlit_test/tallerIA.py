be7d473dfef8c5545d6b492a30a37f17c0405ec3
TheRoger <joserogelioruiz12@gmail.com>
2025-02-26 20:36:24 -0600

    streamlit_test

---
+++ streamlit_test/tallerIA.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Titulo ejemplo")
+
+st.divider()
+
+st.subheader("subheader ejemplo")
+
+st.image("https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRHCCcLRk8wC7I6wOeGJt4d6MPimyZ4h-YlTc2Vlq4KpsVYzAPWGdEwKI4K-jistvqINh8_GKYC_R_ZPkr_JBgbag")
+
+st.text_input("aaaaaaa")
+
+if st.button("enviar"):
+    st.success("si")
+else:
+    st.warning("no") 
